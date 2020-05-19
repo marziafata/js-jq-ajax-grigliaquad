b283342e596ab10e6cd89431e7963fba4723e252
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-19 16:10:22 +0200

    definito this fuori da ajax

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
 
 // Create in HTML una griglia di 36 quadratini (6x6).
-// Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
-// Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
-// In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
-// BONUS: generare la griglia in jQuery utilizzando handlebars
-
-
     for (var i = 0; i < 36; i++) {
         $('#griglia2').append('<div class="quadrato"></div>');
-    }
+    }//fine ciclo for
 
-    $('.quadrato').click(function(){
-            var numero = genera_random(0, 9);
-            if (numero <= 5) {
-                $(this).text(numero).addClass('giallo')
-            } else {
-                $(this).text(numero).addClass('verde');
-            }
-    })
+// Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
 
-    function genera_random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
+$('.quadrato').click(function(){
+    var quadrato_corrente = $(this);
+    $.ajax({
+        'url' : "https://flynn.boolean.careers/exercises/api/random/int",
+        'method' : 'GET',
+        'success' : function(data) {
 
-});
+                        var numero = data.response;
+                        console.log(numero);
+
+                        // Se il numero restituito dall'api è <= 5, scrivo il numero e il quadrato su cui l'utente ha cliccato diventa giallo;
+                        if (numero <= 5) {
+                            quadrato_corrente.text(numero).addClass('giallo')
+                        //se invece il numero restituito dall'api è > 5, scrivo il numero e il quadrato su cui l'utente ha cliccato diventa verde.
+                        } else {
+                            quadrato_corrente.text(numero).addClass('verde');
+                        }//fine if else
+
+                    }//fine function(data)
+
+    });//fine ajax
+
+});//fine funzione click
+
+
+    // BONUS: generare la griglia in jQuery utilizzando handlebars
+
+});//fine document ready
